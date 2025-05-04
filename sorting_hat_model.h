75416c5590367da478acbbca7cc716a6ca5adb4a
Yishuai Zheng <yishuaiz@uw.edu>
2025-05-04 15:09:30 -0700

    first commit

---
+++ sorting_hat_model.h
@@ -0,0 +1,97 @@
+#pragma once
+#include <cstdarg>
+namespace Eloquent {
+    namespace ML {
+        namespace Port {
+            class DecisionTree {
+                public:
+                    /**
+                    * Predict class for features vector
+                    */
+                    int predict(float *x) {
+                        if (x[8] <= 2.5) {
+                            if (x[8] <= 1.5) {
+                                if (x[2] <= 1.5) {
+                                    return 0;
+                                }
+
+                                else {
+                                    if (x[5] <= 1.5) {
+                                        return 0;
+                                    }
+
+                                    else {
+                                        return 1;
+                                    }
+                                }
+                            }
+
+                            else {
+                                if (x[1] <= 2.5) {
+                                    return 1;
+                                }
+
+                                else {
+                                    return 2;
+                                }
+                            }
+                        }
+
+                        else {
+                            if (x[1] <= 3.5) {
+                                if (x[7] <= 3.5) {
+                                    if (x[9] <= 2.5) {
+                                        if (x[0] <= 1.5) {
+                                            return 0;
+                                        }
+
+                                        else {
+                                            return 1;
+                                        }
+                                    }
+
+                                    else {
+                                        return 2;
+                                    }
+                                }
+
+                                else {
+                                    if (x[5] <= 3.5) {
+                                        if (x[5] <= 2.5) {
+                                            return 1;
+                                        }
+
+                                        else {
+                                            return 2;
+                                        }
+                                    }
+
+                                    else {
+                                        return 3;
+                                    }
+                                }
+                            }
+
+                            else {
+                                if (x[2] <= 2.5) {
+                                    if (x[6] <= 3.5) {
+                                        return 2;
+                                    }
+
+                                    else {
+                                        return 3;
+                                    }
+                                }
+
+                                else {
+                                    return 3;
+                                }
+                            }
+                        }
+                    }
+
+                protected:
+                };
+            }
+        }
+    }
